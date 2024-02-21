61a8763a20d1be8cc8752013f22d64d6969c281c
Sergey Boykov <boykov.sergei@gmail.com>
2024-02-22 01:00:34 +0500

    MAX_OBJECT_CNT = 20

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ XMEM_CONFIG = {
 }
 
 # Max possible count of persons in video (if you has error, set bigger number)
-MAX_OBJECT_CNT = 999
+MAX_OBJECT_CNT = 20
 
 # Check new persons in frame every N frames
 YOLO_EVERY = 20
